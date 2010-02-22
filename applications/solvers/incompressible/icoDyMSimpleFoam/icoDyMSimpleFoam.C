26bfd00b120928752da7c36e85d1040771a6e266
hjasak <hjasak@e4e07f05-0c2f-0410-a05a-b8ba57e0c909>
2010-02-22 09:01:49 +0000

    Formatting
    git-svn-id: svn://svn.code.sf.net/p/openfoam-extend/svn/trunk/Core/OpenFOAM-1.5-dev@1667 e4e07f05-0c2f-0410-a05a-b8ba57e0c909

--- applications/solvers/incompressible/icoDyMSimpleFoam/icoDyMSimpleFoam.C
+++ applications/solvers/incompressible/icoDyMSimpleFoam/icoDyMSimpleFoam.C
@@ -125,7 +125,7 @@ int main(int argc, char *argv[])
                 {
                     pEqn.solve(mesh.solver(p.name()));
                 }
-                
+
                 if (nonOrth == nNonOrthCorr)
                 {
                     phi -= pEqn.flux();
