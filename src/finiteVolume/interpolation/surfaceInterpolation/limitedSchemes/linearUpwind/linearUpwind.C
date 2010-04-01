92cb4086b36e855f14fef6afd7939db7da55a351
hjasak <hjasak@e4e07f05-0c2f-0410-a05a-b8ba57e0c909>
2010-04-01 23:02:43 +0000

    Minor clean-up
    git-svn-id: svn://svn.code.sf.net/p/openfoam-extend/svn/trunk/Core/OpenFOAM-1.5-dev@1688 e4e07f05-0c2f-0410-a05a-b8ba57e0c909

--- src/finiteVolume/interpolation/surfaceInterpolation/limitedSchemes/linearUpwind/linearUpwind.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/limitedSchemes/linearUpwind/linearUpwind.C
@@ -95,7 +95,7 @@ Foam::linearUpwind<Type>::correction
 
         if (pSfCorr.coupled())
         {
-            const unallocLabelList& pOwner = 
+            const unallocLabelList& pOwner =
                 mesh.boundary()[patchi].faceCells();
 
             const vectorField& pCf = Cf.boundaryField()[patchi];
@@ -106,7 +106,7 @@ Foam::linearUpwind<Type>::correction
                 gradVf.boundaryField()[patchi].patchNeighbourField();
 
             // Build the d-vectors
-            vectorField pd = 
+            vectorField pd =
                 mesh.Sf().boundaryField()[patchi]
                /(
                    mesh.magSf().boundaryField()[patchi]
@@ -129,7 +129,7 @@ Foam::linearUpwind<Type>::correction
                 }
                 else
                 {
-                    pSfCorr[facei] = 
+                    pSfCorr[facei] =
                         (pCf[facei] - pd[facei] - C[own]) & pGradVfNei[facei];
                 }
             }
